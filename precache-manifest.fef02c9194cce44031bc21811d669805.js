49d285fcf5d6b0fbe60da7926aef1a346f53966f
unknown <serbanalin2006@gmail.com>
2020-08-24 23:09:10 +0300

    Updates

---
+++ precache-manifest.fef02c9194cce44031bc21811d669805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebf2c3026bbaffc8931bf5d82015d82",
+    "url": "/alan-ai-news/index.html"
+  },
+  {
+    "revision": "73aac5da2e79a65b2dd3",
+    "url": "/alan-ai-news/static/css/main.d4d4baae.chunk.css"
+  },
+  {
+    "revision": "2e9d80c5dab5415f70ef",
+    "url": "/alan-ai-news/static/js/2.2a2d68b3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-ai-news/static/js/2.2a2d68b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73aac5da2e79a65b2dd3",
+    "url": "/alan-ai-news/static/js/main.3ccf74d0.chunk.js"
+  },
+  {
+    "revision": "e7ba15125ee979c73433",
+    "url": "/alan-ai-news/static/js/runtime-main.c7cdf677.js"
+  }
+]);
